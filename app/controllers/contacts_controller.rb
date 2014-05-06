3b0127c71d2d18b7209cf995b1b2771a9551f9b7
Corey Norris <coreypnorris@gmail.com>
2014-05-06 10:40:21 -0700

    refactor index method to respond to json requests

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,11 @@
 class ContactsController < ApplicationController
 def index
     @contacts = Contact.all
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @contacts }
+    end
   end
 
   def new
