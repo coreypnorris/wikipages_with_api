e6b1e86779babfd6de337b7b1edefa81cf0e8974
Corey Norris <coreypnorris@gmail.com>
2014-05-06 09:49:11 -0700

    add methods to contactscontroller

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,50 @@
-class Contact < ActiveRecord::Base
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
 
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:notice] = "Contact created."
+      redirect_to contacts_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(contact_params)
+      flash[:notice] = "Contact updated."
+      redirect_to contact_path(@contact)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    flash[:notice] = "Contact deleted."
+    redirect_to contacts_path
+  end
+
+private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :phone)
+  end
 end
